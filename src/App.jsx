b9e5257bb39eca629cdb60626e9bf17ea2faf939
itsutkarsh22 <90923657+itsutkarsh22@users.noreply.github.com>
2023-04-13 17:27:56 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import { Deatils } from './components/Details';
+import { Navbar } from './components/Navbar';
+
+
+function App() {
+
+    return (
+        <div>
+            <Navbar />
+            <Deatils />
+        </div>
+    )
+}
+
+export default App;
+
+
+
